6523b0cc56fc6c3fde864d151cf7cfc93cdf895d
Lisk <liskqwq@gmail.com>
2024-08-22 14:11:47 +0800

    ✨ feat(services): add douyin service

---
+++ src/services/bili.ts
@@ -0,0 +1,32 @@
+import type { Context, Command } from "koishi";
+import { useSource, useImage, useData } from "../hooks";
+
+export const name = "bili";
+
+interface BiliData {
+  data: {
+    list: Array<{
+      position: number;
+      keyword: string;
+      icon: string;
+    }>;
+  };
+}
+
+export async function handler(ctx: Context, hotof: Command) {
+  const source = useSource(name);
+  const image = useImage(ctx.root.config);
+  hotof
+    .subcommand(name)
+    .option("num", "-n <num:number>", { fallback: 10 })
+    .action(async ({ options }) => {
+      const data: BiliData = await fetch(source).then((res) => res.json());
+      return data.data.list
+        .slice(0, options.num)
+        .map(
+          (a) =>
+            `=${a.position}= ${a.keyword}\n${a.icon ? image(a.icon) : "(暂无图片捏)"}\n`,
+        )
+        .join("");
+    });
+}
---
+++ src/services/douyin.ts
@@ -0,0 +1,51 @@
+import { Context, Command } from "koishi";
+import { useSource, useImage, useData } from "../hooks";
+
+export const name = "douyin";
+
+interface DouyinData {
+  data: {
+    word_list: Array<{
+      position: number;
+      word: string;
+      hot_value: number;
+      video_count: number;
+      word_cover: {
+        url_list: string[];
+      };
+    }>;
+    trending_list: Array<{
+      word: string;
+      hot_value: number;
+      video_count: number;
+      word_cover: { url_list: string[] };
+    }>;
+  };
+}
+
+export async function handler(ctx: Context, hotof: Command) {
+  const source = useSource(name);
+  const image = useImage(ctx.root.config);
+  hotof
+    .subcommand(name)
+    .option("num", "-n <num:number>", { fallback: 10 })
+    .action(async ({ options }) => {
+      const data: DouyinData = await fetch(source).then((res) => res.json());
+      const hots = data.data.word_list
+        .slice(0, options.num)
+        .map(
+          (a) =>
+            `=${a.position}= ${a.word}(${a.hot_value / 10000 + "w"}, 视频${a.video_count})\n${a.word_cover.url_list[0] ? image(a.word_cover.url_list[0]) : "(暂无图片捏)"}\n`,
+        )
+        .join("");
+      const trendings =
+        "实时上升热点\n" +
+        data.data.trending_list
+          .slice(0, options.num)
+          .map(
+            (a) =>
+              `+ ${a.word}(视频${a.video_count})\n${a.word_cover.url_list[0]}\n`,
+          );
+      return `■热点榜■\n${hots}\n■实时上升热点■${trendings}`;
+    });
+}
