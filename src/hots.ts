d981dfd0fbca36b7666ddaeaad979d7679f6996e
Lisk <liskqwq@gmail.com>
2024-08-22 14:12:44 +0800

    ✨ feat(core): do not create instances immediately.

--- src/hots.ts
+++ src/hots.ts
@@ -1,46 +1,50 @@
-import * as services from './services'
-import {config} from './index';
+import { isEmpty } from "./utils";
 
+import type {Config} from "./index"
 
-import type {Config} from './index';
-import {} from 'koishi';
+import { Context, Command } from "koishi";
 
-type service = {name:string, handler:(this:Context)=>void}
+type service = {
+  name: string;
+  status?: "ok" | "error";
+  handler: (ctx: Context, hotof: Command) => void;
+};
 
-class Hots{
-  services:Service[]|[]=[];
-  constructor(internal){
-    internal.forEach(({name, handler})=>{
-      this.define(name, handler)
-    })
+export class Hots {
+  services: service[];
+  config: Config;
+  constructor(internal) {
+    this.services = [];
+    internal.forEach(({ name, handler }) => {
+      this.define(name, handler);
+    });
   }
-  define(name, handler){
-    this.services.push({name,handler})
+  define(name: string, handler: (ctx: Context, hotof: Command) => void) {
+    this.services.push({ name, handler });
   }
-  async apply(ctx, name, handler, hotof){
-    handler.apply(ctx, hotof)
+  async apply(ctx, name, handler, hotof) {
+    handler.call(ctx, hotof);
   }
-  initialze(instance){
-    instance.usage("Get platform hot search!")
-    instance.example("hotof.bili -i -n=10")
+  initialize(instance) {
+    instance.usage("Get platform hot search!");
+    instance.example("hotof.bili -i -n=10");
   }
-  use(ctx){
-    const hotof=ctx.command(ctx.root.config.command)
-    initialize(hotof)
-    
+  use(ctx) {
+    this.config = ctx.root.config;
+    const hotof = ctx.command(ctx.root.config.command);
+    this.initialize(hotof);
+    this.mounted(hotof, ctx);
   }
-  mounted(hotof, ctx){
-    if(isEmpty(services)) throw new TypeError("services list is empty")
-    this.services.forEach(({name, handler}, index)=>{
-      try{
-        this.apply(ctx, name, handler, hotof)
-      } catch(e){
-	this.services[index].status="error"
+  mounted(hotof, ctx) {
+    if (!this.services.length) throw new TypeError("services list is empty");
+    this.services.forEach(({ name, handler }, index) => {
+      try {
+        this.apply(ctx, name, handler, hotof);
+      } catch (e) {
+        this.services[index].status = "error";
         throw e;
       }
-      this.services[index].status="ok"
-    })
+      this.services[index].status = "ok";
+    });
   }
 }
-
-export const hots = new Hots(Object.values(services))
