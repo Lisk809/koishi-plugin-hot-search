936162665f81ca5ba72cf4e375b0a21dab7b4a24
Lisk <liskqwq@gmail.com>
2024-08-24 21:21:30 +0800

    ✨ feat(services): add zhihu, weibo service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,4 @@
 export * as bili from "./bili";
 export * as douyin from "./douyin";
-//export * as weibo from "./weibo"
-//export * as zhihu from "./zhihu"
+export * as weibo from "./weibo"
+export * as zhihu from "./zhihu"
---
+++ src/services/zhihu.ts
@@ -0,0 +1,26 @@
+import type { Context, Command } from "koishi";
+import type { Config } from "../index"
+
+import { useSource, useImage } from "../hooks";
+
+export const name = "zhihu";
+
+interface ZhihuData {
+  top_search: {
+    words: Array<{
+      display_query: string
+    }>;
+  };
+}
+
+export async function handler(ctx: Context, config: Config) {
+  const source = useSource(config, name);
+  const image = useImage(config);
+  ctx
+    .command("hotof."+name)
+    .option("num", "-n <num:number>", { fallback: 10 })
+    .action(async ({ options }) => {
+      const data: ZhihuData = await fetch(source).then((res) => res.json());
+      return "知乎热搜🔥\n"+data.top_search.words.slice(0, options.num).map(a=>`${a.display_query}`).join("\n")
+    });
+}
